f904db469eb6a2a148509cc1e43f89cd43bb35e1
Justin Parker <justin.parker@altec.com>
2017-03-16 16:12:37 -0500

    added update api

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, abort
 import json, os
 
 app = Flask(__name__)
+
 
 def readFromFile(fileName):
 	my_dir = os.path.dirname(__file__)
@@ -11,10 +12,20 @@ def readFromFile(fileName):
 		data = json.load(data_file)
 		return data
 
+
+def writeToFile(fileName, data):
+	my_dir = os.path.dirname(__file__)
+	file_path = os.path.join(my_dir, 'data/%s.json' % fileName)
+
+	with open(file_path, "w") as jsonFile:
+		json.dump(data, jsonFile)
+
+
 @app.route("/api/players")
 def getPlayers():
 	players = readFromFile('players')
 	return jsonify(list(players.keys()))
+
 
 @app.route("/api/rnd/<int:rnd>")
 def getRound(rnd):
@@ -48,10 +59,68 @@ def getRound(rnd):
 
 	return jsonify(result)	
 
+
 @app.route("/")
 def index():
-	 return render_template('index.html')
+	return render_template('index.html')
+
+
+@app.route("/api/update/<player_name>/<int:rnd>/<region>/<int:seed>")
+def updateTables(player_name, rnd, region, seed):
+	team_id, team = getTeamBySeed(region, seed)
+	updatePlayerFile(player_name, rnd, team_id)
+	updateRoundFile(region, rnd, seed)
+
+	return 'got em'
+
+
+def updateRoundFile(region, rnd, seed):
+	round_str = "round%s" % rnd
+	rounds = readFromFile('rounds')
+
+	if (rnd == 2):
+		if (seed == 1 or seed == 16):
+			matchup, slot = 0, 0
+		if (seed == 8 or seed == 9):
+			matchup, slot = 0, 1
+		if (seed == 5 or seed == 12):
+			matchup, slot = 1, 0
+		if (seed == 4 or seed == 13):
+			matchup, slot = 1, 1
+
+		if (seed == 6 or seed == 11):
+			matchup, slot = 2, 0
+		if (seed == 3 or seed == 14):
+			matchup, slot = 2, 1
+		if (seed == 7 or seed == 10):
+			matchup, slot = 3, 0
+		if (seed == 2 or seed == 15):
+			matchup, slot = 3, 1
+
+	rounds[region][round_str][matchup][slot] = seed
+	writeToFile('rounds', rounds)
+
+def updatePlayerFile(player, rnd, team_id):
+	round_str = "round%s" % rnd
+	players = readFromFile('players')
+
+	players[player][round_str].append(team_id)
+
+	writeToFile('players', players)
+
+
+def getTeamBySeed(region, seed):
+	teams = readFromFile('teams')
+	teamList = teams.get(region)
+	for id, curr_team in teamList.items():
+		if curr_team[1] == seed:
+			team_id = id
+			team = curr_team
+			break
+
+	return int(team_id), team
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5050)
+	app.run(host='0.0.0.0', port=5050)
 
